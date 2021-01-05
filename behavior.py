e3e444b5201e56bff1863ac57b36a9cfccae6101
Vinicius Sueiro <hi@vsueiro.com>
2021-01-05 18:58:30 -0300

    Tries to use clouds for the chart

--- behavior.py
+++ behavior.py
@@ -34,9 +34,9 @@ has_just_vibrated = False # create flag to prevent long vibrations (if humidity 
 legend  = '\n'
 legend += 'How to read the chart?\n'
 legend += '\n'
-legend += '💧💧💧💧💧💧💧💧|💧💧💧💧 75% ← Humidity in %\n'
-legend += '💧💧💧💧💧💧💧💧|💧💧💧 70% 🌿 ← Plant vibration triggered\n'
-legend += '💧💧💧💧💧💧💧💧| ← Baseline*\n'
+legend += '☁️☁️☁️☁️☁️☁️☁️☁️🌧️🌧️🌧️🌧️ 75% ← Humidity in %\n'
+legend += '☁️☁️☁️☁️☁️☁️☁️☁️🌧️🌧️🌧️ 70% 🌿 ← Plant vibration triggered\n'
+legend += '☁️☁️☁️☁️☁️☁️☁️☁️ ← Baseline*\n'
 legend += '\n'
 legend += '*After crossing this line, the first decrease in humidity will trigger a vibration.'
 legend += '\n'
@@ -112,9 +112,9 @@ while True:
 
       # draw bar “chart” (one drop per % point) with baseline:
       if rounded >= baseline:
-        bar = '●' * baseline + '|' + '●' * (rounded - baseline)
+        bar = '☁️' * baseline + '|' + '🌧️' * (rounded - baseline)
       else:
-        bar = '●' * rounded + ' ' * (baseline - rounded) + '|'
+        bar = '☁️' * rounded + ' ' * (baseline - rounded) + '|'
 
       label = str(rounded) + '%'
 
