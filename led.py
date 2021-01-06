7a49c7234aeeb746da57795a260d7c35b92fa3ca
Vinicius Sueiro <hi@vsueiro.com>
2021-01-06 16:52:34 -0300

    Tries to make base function run in the backgroun (not to startover on each while True loop)

--- led.py
+++ led.py
@@ -6,40 +6,45 @@ base = PWMLED(22)
 
 last_interaction = time.time()
 
-#needy_level_updated = False
+current_needy_level = None
 
-def needy( level ):
+async def needy(level):
 
-  if level == 0:
-    base.value = .25
+  if level != current:
 
-  if level == 1:
-  	base.pulse(fade_in_time=2, fade_out_time=2)
+    if level == 0:
+      base.value = .25
+  
+    elif level == 1:
+      base.pulse(fade_in_time=2, fade_out_time=2)
+  
+    elif level == 2:
+      base.pulse(fade_in_time=1, fade_out_time=1)
+  
+    elif level == 3:
+      base.blink()
 
-  if level == 2:
-  	base.pulse(fade_in_time=1, fade_out_time=1)
-
-  if level == 3:
-  	base.blink()
+    # adding 0 to make a copy, not to link them always to the same value
+    current_needy_level = level + 0 
 
 
 while True:
 
   if elapsed > 0:
-  	# chillin’
+    # chillin’
     needy( 0 )
 
   elif elapsed > 10:
-  	# missing u
+    # missing u
   	needy( 1 ) 
 
   elif elapsed > 20:
-  	# why don’t you love me anymore?
+    # why don’t you love me anymore?
   	needy( 2 ) 
 
   elif elapsed > 20:
-  	# FUCKING TALK TO ME
-  	needy( 3 ) 
+    # FUCKING TALK TO ME
+    needy( 3 ) 
 
   now = time.time()
   elapsed = now - last_interaction
