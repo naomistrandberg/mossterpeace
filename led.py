f23c4a7c8174fb84ea906af1b754688289b80e51
Vinicius Sueiro <hi@vsueiro.com>
2021-01-06 16:06:32 -0300

    Adds built-in pulse function

--- led.py
+++ led.py
@@ -1,28 +1,9 @@
 from gpiozero import PWMLED
 from time import sleep
+from signal import pause
 
 led = PWMLED(22)
 
-while True:
-    led.value = 0
-    sleep(.25)
-    led.value = 0.1
-    sleep(.25)
-    led.value = 0.2
-    sleep(.25)
-    led.value = 0.3
-    sleep(.25)
-    led.value = 0.4
-    sleep(.25)
-    led.value = 0.5
-    sleep(.25)
-    led.value = 0.6
-    sleep(.25)
-    led.value = 0.7
-    sleep(.25)
-    led.value = 0.8
-    sleep(.25)
-    led.value = 0.9
-    sleep(.25)
-    led.value = 1
-    sleep(.25)
+pulse(fade_in_time=1, fade_out_time=1)
+
+pause()
