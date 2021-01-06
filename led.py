f734a7cd48afa757f96bf4fa71dc7089d685dbc1
Vinicius Sueiro <hi@vsueiro.com>
2021-01-06 17:15:22 -0300

    Adds some comments

--- led.py
+++ led.py
@@ -9,28 +9,29 @@ last_interaction = time.time()
 current_needy_level = -1
 
 def needy(level):
-
+  
+  # makes sure we’re changing the global variable (not creating a new local one)
   global current_needy_level
 
   if level != current_needy_level:
 
     if level == 0:
+      # set base to light up with .25 intensity
       base.value = .25
-      print( 'set base to light up with .25 intensity' )
-  
-    elif level == 1:
-      base.pulse(fade_in_time=2, fade_out_time=2)
-      print( 'set base light to pulsate slowly' )
-  
-    elif level == 2:
-      base.pulse(fade_in_time=1, fade_out_time=1)
-      print( 'set base light to pulsate fastly' )
-  
-    elif level == 3:
-      base.blink()
-      print( 'set base light to blink' )
 
-    # adding 0 to make a copy, not to link them always to the same value
+    elif level == 1:
+      # set base light to pulsate slowly
+      base.pulse(fade_in_time=2, fade_out_time=2)
+
+    elif level == 2:
+      # set base light to pulsate fastly
+      base.pulse(fade_in_time=1, fade_out_time=1)
+
+    elif level == 3:
+      # set base light to blink
+      base.blink()
+
+    # adding 0 to copy the value, otherwise both variables would always point to the same value
     current_needy_level = level + 0 
 
 while True:
